5e30e6e01735f41768ce7d03cf9b0e97dd39b055
mg <mg@mg>
2022-03-19 19:24:49 -0400

    Code and test done for second_tests

--- second_tests/functions.js
+++ second_tests/functions.js
@@ -6,6 +6,8 @@ class Book {
     this.title = title
     this.pageNumbers = pageNumbers
     this.genre = genre
+    this.borrowed = false
+
   }
 }
 class Shelf {
@@ -37,12 +39,54 @@ class SortedShelf extends Shelf {
     this.books.sort(this.sortingFunction)
   }
 }
-
+function UndefinedUser(message) {
+    this.message = message;
+    this.name = 'UndefinedUser';
+  }
+  function NoSuchBookOnShelf(message) {
+    this.message = message;
+    this.name = 'NoSuchBookOnShelf';
+  }
 class Library {
   constructor () {
     this.shelfs = []
+    this.users = new Set()
   }
 
+  addUser (user) {
+    this.users.add(user)
+  }
+  borrowABook (user,_book) {
+      if(false === this.users.has(user) )
+      {
+        throw new UndefinedUser(`There is no such user with id = ${user.id}`)
+        return
+      }
+
+      var bookFound = false
+      for(let i = 0 ; i < this.shelfs.length ; i++)
+      {
+        console.log(this.shelfs[i].books)
+
+        const filteredBooks = (this.shelfs[i]).books.filter((item) => { return item === _book })
+
+        if(filteredBooks.length!=0)
+        {
+            console.log(this.shelfs)
+
+            bookFound = true
+            break
+        }
+
+      }
+      if(!bookFound)
+      {
+        throw new NoSuchBookOnShelf(`There is no such book in library with title = ${_book.title}`)
+      }
+
+    user.books.push(_book)
+  }
+  
   addShelf (shelf) {
     this.shelfs.push(shelf)
   }
@@ -58,16 +102,77 @@ class Library {
     return books
   }
 
-  getBookByISBN () {
-
+  getBookByAuthorRegex (regexAuthor) {
+    const books = []
+    for (let i = 0; i < this.shelfs.length; i++) {
+      const filteredBooks = (this.shelfs[i]).books.filter((item) => { return item.author.match(regexAuthor) })
+      for (let k = 0; k < filteredBooks.length; k++) {
+        books.push(filteredBooks[k])
+      }
+    }
+    return books
   }
 
-  getBookByGenre () {
-
+  getBookByISBN (searchedISBN) {
+    const books = []
+    for (let i = 0; i < this.shelfs.length; i++) {
+      const filteredBooks = (this.shelfs[i]).books.filter((item) => { return item.ISBN === searchedISBN })
+      for (let k = 0; k < filteredBooks.length; k++) {
+        books.push(filteredBooks[k])
+      }
+    }
+    return books
   }
 
-  getBookByTitle () {
+  getBookByGenre (searchedGenre) {
+    const books = []
+    for (let i = 0; i < this.shelfs.length; i++) {
+      const filteredBooks = (this.shelfs[i]).books.filter((item) => { return item.genre === searchedGenre })
+      for (let k = 0; k < filteredBooks.length; k++) {
+        books.push(filteredBooks[k])
+      }
+    }
+    return books
+  }
 
+  getBookByGenreRegex (searchedGenre) {
+    const books = []
+    for (let i = 0; i < this.shelfs.length; i++) {
+      const filteredBooks = (this.shelfs[i]).books.filter((item) => { return item.genre.match(searchedGenre) })
+      for (let k = 0; k < filteredBooks.length; k++) {
+        books.push(filteredBooks[k])
+      }
+    }
+    return books
+  }
+
+  getBookByTitle (searchedTitle) {
+    const books = []
+    for (let i = 0; i < this.shelfs.length; i++) {
+      const filteredBooks = (this.shelfs[i]).books.filter((item) => { return item.title === (searchedTitle) })
+      for (let k = 0; k < filteredBooks.length; k++) {
+        books.push(filteredBooks[k])
+      }
+    }
+    return books
+  }
+
+  getBookByTitleRegex (searchedTitle) {
+    const books = []
+    for (let i = 0; i < this.shelfs.length; i++) {
+      const filteredBooks = (this.shelfs[i]).books.filter((item) => { return item.title.match(searchedTitle) })
+      for (let k = 0; k < filteredBooks.length; k++) {
+        books.push(filteredBooks[k])
+      }
+    }
+    return books
+  }
+}
+class User {
+    static _id = 0
+  constructor () {
+    this.id = User._id++
+    this.books = []
   }
 }
 
@@ -76,3 +181,8 @@ module.exports.Book = Book
 module.exports.Shelf = Shelf
 module.exports.SortedShelf = SortedShelf
 module.exports.Library = Library
+module.exports.User = User
+module.exports.UndefinedUser = UndefinedUser
+module.exports.NoSuchBookOnShelf = NoSuchBookOnShelf
+
+
--- second_tests/functions.test.js
+++ second_tests/functions.test.js
@@ -9,6 +9,8 @@ test('Book creation', () => {
   expect(anny).toHaveProperty('pageNumbers')
   expect(anny).toHaveProperty('genre')
 
+  expect(anny).toHaveProperty('genre')
+
   expect(anny.ISBN).toEqual(1234)
   expect(anny.author).toMatch('Lucy Maud Montgomery')
   expect(anny.title).toMatch('Anne of Green Gables')
@@ -22,6 +24,11 @@ test('Add to shelf', () => {
   const b3 = new functions.Book(123, 'Spot', 'How to meow', 656, 'Animals')
 
   const s = new functions.Shelf()
+
+  expect(s).toHaveProperty('addBook')
+  expect(s).toHaveProperty('getAllBy')
+  expect(s).toHaveProperty('books')
+
   s.addBook(b)
   expect(s.books.length).toEqual(1)
   expect(s.books[0]).toStrictEqual(b)
@@ -39,6 +46,8 @@ test('Remove from shelf', () => {
   const b3 = new functions.Book(123, 'Spot', 'How to meow', 656, 'Animals')
 
   const s = new functions.Shelf()
+  expect(s).toHaveProperty('removeBook')
+
   s.addBook(b)
   s.addBook(b2)
   s.addBook(b3)
@@ -105,6 +114,32 @@ test('Sorted shelf', () => {
   const b32 = new functions.Book(3361, 'Spot', 'How to morph into lizard', 774, 'Animals behaviour')
 
   const s = new functions.SortedShelf((first, second) => { return first.pageNumbers - second.pageNumbers })
+  expect(functions.SortedShelf.prototype).toBeInstanceOf(functions.Shelf)
+
+  expect(s).toHaveProperty('removeBook')
+  expect(s).toHaveProperty('addBook')
+  expect(s).toHaveProperty('getAllBy')
+  expect(s).toHaveProperty('sortingFunction')
+  expect(s).toHaveProperty('books')
+  {
+    const sortFunctionMock = jest.fn((first, second) => { return true })
+
+    const sM = new functions.SortedShelf(sortFunctionMock)
+    expect(sM.sortingFunction).toEqual(sortFunctionMock)
+
+    sM.addBook(b)
+    expect(sortFunctionMock).toHaveBeenCalledTimes(0)
+    sM.addBook(b2)
+    expect(sortFunctionMock).toHaveBeenCalledTimes(1)
+    sM.addBook(b3)
+    expect(sortFunctionMock).toHaveBeenCalledTimes(3)
+    sM.addBook(b31)
+    expect(sortFunctionMock).toHaveBeenCalledTimes(6)
+    sM.addBook(b32)
+    expect(sortFunctionMock).toHaveBeenCalledTimes(10)
+    sM.addBook(b21)
+    expect(sortFunctionMock).toHaveBeenCalledTimes(15)
+  }
   s.addBook(b)
   s.addBook(b2)
   s.addBook(b3)
@@ -174,11 +209,187 @@ test('Library test', () => {
   s.addBook(b21)
 
   const l = new functions.Library()
-  l.addShelf(s)
-  const dataBooks = l.getBookByAuthor('Data')
-  expect(dataBooks.length).toEqual(2)
-  expect(dataBooks[0]).toStrictEqual(b2)
-  expect(dataBooks[1]).toStrictEqual(b21)
+  expect(l).toHaveProperty('shelfs')
+  expect(l).toHaveProperty('addShelf')
+  expect(l).toHaveProperty('getBookByAuthor')
+  expect(l).toHaveProperty('getBookByISBN')
+  expect(l).toHaveProperty('getBookByGenre')
+  expect(l).toHaveProperty('getBookByTitle')
 
+  l.addShelf(s)
+  {
+    const dataBooks = l.getBookByAuthor('Data')
+    expect(dataBooks.length).toEqual(2)
+    expect(dataBooks[0]).toStrictEqual(b2)
+    expect(dataBooks[1]).toStrictEqual(b21)
+  }
+  {
+    const dataBooks = l.getBookByAuthor('Udhgfa')
+    expect(dataBooks.length).toEqual(0)
+  }
+  {
+    const isbnBooks = l.getBookByISBN(1253)
+    expect(isbnBooks.length).toEqual(1)
+    expect(isbnBooks[0]).toStrictEqual(b3)
+  }
+  {
+    const isbnBooks = l.getBookByISBN(55556)
+    expect(isbnBooks.length).toEqual(0)
+  }
+  {
+    const genreBooks = l.getBookByGenre('Animals behaviour')
+    expect(genreBooks.length).toEqual(3)
+    expect(genreBooks).toEqual(expect.arrayContaining([b3, b31, b32]))
+  }
+  {
+    const genreBooks = l.getBookByGenre('undna')
+    expect(genreBooks.length).toEqual(0)
+  }
+  {
+    const genreBooks = l.getBookByGenre('Animals behaviour')
+    expect(genreBooks.length).toEqual(3)
+    expect(genreBooks).toEqual(expect.arrayContaining([b3, b31, b32]))
+  }
+  {
+    const titleBooks = l.getBookByTitle('How to morph into lizard')
+    expect(titleBooks.length).toEqual(1)
+    expect(titleBooks[0]).toStrictEqual(b32)
+  }
+})
+test('Library Regex test', () => {
+  const b = new functions.Book(3315, 'Jorgdie La Forge', 'Hyperspace Introduction', 331, 'Technical')
+  const b2 = new functions.Book(4415, 'Data', 'InterHuman Communications', 52, 'Physio')
+  const b21 = new functions.Book(5526, 'Data', 'Positronic brain schematics', 246, 'Technical')
+
+  const b3 = new functions.Book(1253, 'Spot', 'How to meow', 656, 'Animals behaviour')
+  const b31 = new functions.Book(5762, 'Spot', 'How to hiss', 441, 'Animals behaviour')
+  const b32 = new functions.Book(3361, 'Spot', 'How to morph into lizard', 774, 'Animals behaviour')
+  const b4 = new functions.Book(4412, 'Spoc', 'Overcome Your emotions', 554, 'Physio')
+
+  const s = new functions.SortedShelf((first, second) => { return first.pageNumbers - second.pageNumbers })
+  s.addBook(b)
+  s.addBook(b2)
+  s.addBook(b3)
+  s.addBook(b31)
+  s.addBook(b4)
+  s.addBook(b32)
+  s.addBook(b21)
+
+  const l = new functions.Library()
+  expect(l).toHaveProperty('getBookByAuthorRegex')
+  expect(l).toHaveProperty('getBookByTitleRegex')
+  expect(l).toHaveProperty('getBookByGenreRegex')
+  l.addShelf(s)
+  {
+    const dataBooks = l.getBookByAuthorRegex(/Spo/)
+    expect(dataBooks.length).toEqual(4)
+    expect(dataBooks).toEqual(expect.arrayContaining([b3, b31, b32, b4]))
+  }
+  {
+    const dataBooks = l.getBookByTitleRegex(/Int/i)
+    expect(dataBooks.length).toEqual(3)
+    expect(dataBooks).toEqual(expect.arrayContaining([b, b2, b32]))
+  }
+  {
+    const dataBooks = l.getBookByGenreRegex(/io/i)
+    expect(dataBooks.length).toEqual(5)
+    expect(dataBooks).toEqual(expect.arrayContaining([b3, b31, b32, b4, b3]))
+  }
+})
+test('LibraryUser id', () => {
+  const l = new functions.Library()
+  expect(l).toHaveProperty('users')
+
+  expect(l.users).toBeInstanceOf(Set)
+  const JKRowloong = new functions.User()
+  expect(JKRowloong).toHaveProperty('id', 0)
+  const JKRowloong1 = new functions.User()
+  expect(JKRowloong1).toHaveProperty('id', 1)
+  const JKRowloong2 = new functions.User()
+  expect(JKRowloong2).toHaveProperty('id', 2)
+  const JKRowloong3 = new functions.User()
+  expect(JKRowloong3).toHaveProperty('id', 3)
+  expect(JKRowloong).toHaveProperty('id', 0)
+  expect(JKRowloong1).toHaveProperty('id', 1)
+  expect(JKRowloong2).toHaveProperty('id', 2)
+})
+test('LibraryUser addUnique users', () => {
+  const b = new functions.Book(3315, 'Jorgdie La Forge', 'Hyperspace Introduction', 331, 'Technical')
+  const b2 = new functions.Book(4415, 'Data', 'InterHuman Communications', 52, 'Physio')
+  const b21 = new functions.Book(5526, 'Data', 'Positronic brain schematics', 246, 'Technical')
+
+  const b3 = new functions.Book(1253, 'Spot', 'How to meow', 656, 'Animals behaviour')
+  const b31 = new functions.Book(5762, 'Spot', 'How to hiss', 441, 'Animals behaviour')
+  const b32 = new functions.Book(3361, 'Spot', 'How to morph into lizard', 774, 'Animals behaviour')
+  const b4 = new functions.Book(4412, 'Spoc', 'Overcome Your emotions', 554, 'Physio')
+
+  const s = new functions.Shelf()
+  s.addBook(b)
+  s.addBook(b2)
+  s.addBook(b3)
+  s.addBook(b31)
+  s.addBook(b4)
+  s.addBook(b32)
+  s.addBook(b21)
+
+  const l = new functions.Library()
+  l.addShelf(s)
+  expect(l).toHaveProperty('addUser')
+
+  const JKRowloong = new functions.User()
+  const JKRowloong1 = new functions.User()
+  const JKRowloong2 = new functions.User()
+  const JKRowloong3 = new functions.User()
+  l.addUser(JKRowloong)
+  l.addUser(JKRowloong1)
+  l.addUser(JKRowloong2)
+  l.addUser(JKRowloong3)
+  l.addUser(JKRowloong2)
+
+  l.addUser(JKRowloong3)
+  l.addUser(JKRowloong2)
+
+  expect(Array.from(l.users)).toEqual(expect.arrayContaining([JKRowloong, JKRowloong1, JKRowloong2, JKRowloong3]))
+  expect(l.users.size).toEqual(4)
+})
+test('LibraryUser getBooks for user', () => {
+  const b = new functions.Book(3315, 'Jorgdie La Forge', 'Hyperspace Introduction', 331, 'Technical')
+  const b2 = new functions.Book(4415, 'Data', 'InterHuman Communications', 52, 'Physio')
+  const b21 = new functions.Book(5526, 'Data', 'Positronic brain schematics', 246, 'Technical')
+
+  const b3 = new functions.Book(1253, 'Spot', 'How to meow', 656, 'Animals behaviour')
+  const b31 = new functions.Book(5762, 'Spot', 'How to hiss', 441, 'Animals behaviour')
+  const b32 = new functions.Book(3361, 'Spot', 'How to morph into lizard', 774, 'Animals behaviour')
+  const b4 = new functions.Book(4412, 'Spoc', 'Overcome Your emotions', 554, 'Physio')
+
+  const s = new functions.Shelf()
+  s.addBook(b2)
+  s.addBook(b3)
+  s.addBook(b31)
+  s.addBook(b4)
+  s.addBook(b32)
+  s.addBook(b21)
+
+  const l = new functions.Library()
+  l.addShelf(s)
+  expect(l).toHaveProperty('addUser')
+
+  const JKRowloong = new functions.User()
+  const JKRowloong1 = new functions.User()
+  const JKRowloong2 = new functions.User()
+  const JKRowloong3 = new functions.User()
+  const ChingPing = new functions.User()
+
+  l.addUser(JKRowloong)
+  l.addUser(JKRowloong1)
+  l.addUser(JKRowloong2)
+  l.addUser(JKRowloong3)
+  l.addUser(JKRowloong3)
+  expect(l).toHaveProperty('borrowABook')
+
+  expect(() => l.borrowABook(ChingPing, b)).toThrow(functions.UndefinedUser)
+  expect(() => l.borrowABook(JKRowloong, b)).toThrow(functions.NoSuchBookOnShelf)
+  s.addBook(b)
+  expect(() => l.borrowABook(JKRowloong, b)).not.toThrow(functions.NoSuchBookOnShelf)
 
 })
